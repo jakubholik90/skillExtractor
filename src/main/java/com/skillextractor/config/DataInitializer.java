6ea8462ffd6c471806da4c69b2b19db58916be1b
Jakub Holik <jakubholik90@gmail.com>
2025-10-26 00:06:57 +0200

    add DataInitializer.java

---
+++ src/main/java/com/skillextractor/config/DataInitializer.java
@@ -0,0 +1,17 @@
+package com.skillextractor.config;
+
+import com.skillextractor.model.User;
+import com.skillextractor.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class DataInitializer {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+}
